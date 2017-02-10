9e5c6e659c192308f2e12cef0a8a625fe5416d54
nhimlicy <nhimlicy95pt@gmail.com>
2017-02-10 16:40:41 +0700

    question

--- online_exam/app/questionCtrl.js
+++ online_exam/app/questionCtrl.js
@@ -3,7 +3,20 @@ app.controller('questionCtrl', function ($scope, $modal, $filter, Data){
 	  Data.get('questions').then(function(data){
       $scope.questions = data;
     });
+<<<<<<< HEAD
     Data.get('answers').then(function(data){
       $scope.answers = data;
    });
+=======
+
+`id`, `content`, `number_answer`, `number_question_true`, `category_id`, `section_id`, `type`
+
+    $scope.columns = [
+                    {text:"ID",predicate:"id",sortable:true,dataType:"number"},
+                    {text:"Content",predicate:"content",sortable:true},
+                    {text:"Number answer",predicate:"number_answer",sortable:true},
+                    {text:"Number question true",predicate:"number_question_true",sortable:true},
+                   
+                ];
+>>>>>>> question
 })
