eed5f5dd8bda4284ff6b9e296d95722dd68446a6
Nguyen Phan <phannh.uetk58@gmail.com>
2017-02-24 10:10:51 +0700

    create exam

--- online_exam/app/categoryCtrl.js
+++ online_exam/app/categoryCtrl.js
@@ -3,4 +3,28 @@ app.controller('categoryCtrl', function ($scope, $modal, $filter, Data) {
     Data.get('categories').then(function(data){
         $scope.categories = data;
     });
+
+    $scope.columns = [
+                    {text:"Name",predicate:"name",sortable:true,dataType:"number"},
+                    {text:"Descriptions",predicate:"Descriptions",sortable:true},
+                ];
+})
+
+app.controller('editCategoryCtrl', function ($scope, $modalInstance, item, Data){
+  $scope.category = angular.copy(item);
+
+  $scope.cancel = function(){
+    $modalInstance.dismiss('Close');
+  };
+  $scope.title = 'New Category';
+  $scope.buttonText = "Add New Category"
+
+  var original = item;
+  $scope.isClean = function(){
+    return angular.equals(original, $scope.category);
+  }
+  $scope.saveCategory = function(category) {
+    category =
+  }
+
 })
